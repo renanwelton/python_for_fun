fd32c129ad20ae4f85333daee0816fa8c78d3dc6
Renan Welton <renan.weltonrwh@gmail.com>
2023-06-18 15:39:04 -0300

    no more global list

--- perfect_num.py
+++ perfect_num.py
@@ -1,9 +1,7 @@
-divs = []
-
 def ensure_int(ask):
     """ Works like input(), but ensures the input is an integer > 0. """
     while True:
-        print("\n" + ask, end = " ")
+        print(f"\n{ask}", end=" ")
         try:
             num = int(input())
             if num < 1:
@@ -15,28 +13,29 @@ def ensure_int(ask):
 
 def prop_divs(num):
     """ Finds all whole divisors of a number, including 1. """
+    divs = []
     for n in range(1, num//2+1):
         if num % n == 0:
             divs.append(n)
     return divs
 
-def perf_num(num):
-    return sum(prop_divs(num)) == num
+def perf_num(num, divs):
+    return sum(divs) == num
 
 def main():
     print("\nLet's find perfect numbers.")
     num = ensure_int("Type a positive number:")
-    
-    if perf_num(num):
-        print("\nYes,", num, "is a perfect number.\nIt's divisors are:", end = " ")
+    divs = prop_divs(num)
+    if perf_num(num, divs):
+        print(f"\nYES, {num} is a perfect number.\nIt's divisors are:", end=" ")
         for n in divs:
             if not n == divs[-1]:
-                print(n, end = ", ")
+                print(n, end=", ")
             else:
-                print(str(divs[-1]) + ".")
+                print(f"{divs[-1]}.")
         print()
     else:
-        print("\nNo,", num, "is not a perfect number.\n")
+        print(f"\nNO, {num} is not a perfect number.\n")
 
     question = input("Wanna try again? (Y/n) ")
     if question in ["Yes", "yes", "Y", "y", ""]:
@@ -45,12 +44,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    ## Debug code ##
-
-    """num = 1
-    while True:
-        result, divisors = perf_num(num)
-        if result:
-            print(num)
-        num += 1"""
