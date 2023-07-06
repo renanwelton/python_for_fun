464947496a51052836fbbb02570473283de0b7b7
Renan Welton <renan.weltonrwh@gmail.com>
2023-07-06 18:05:14 -0300

    fixed some coding

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -1,17 +1,15 @@
-def roman_integer(rnum: str):
+def roman_integer(roman_string):
     """Receives a roman number as a string. Returns an integer."""
     roman_nums = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
     nums = []
-    for letter in rnum.upper():
+    for letter in roman_string:
         nums.append(roman_nums[letter])
     res = 0
     while len(nums) > 1:
-        if nums[0] >= nums[1]:
+        if len(nums) == 1 or nums[0] >= nums[1]:
             res += nums[0]
             del nums[0]
         else:
             res += (nums[1] - nums[0])
             del nums[0:2]
-    if len(nums) == 1:
-        res += nums[0]
     return res
